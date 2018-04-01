aca37379162ef8fca8a08dabbc492689773c6747
ssor <ssor@qq.com>
2018-04-01 15:26:19 +0800

    add cli

--- main.go
+++ main.go
@@ -4,24 +4,78 @@ import (
 	"fmt"
 	"blockchain/core"
 	"github.com/boltdb/bolt"
+	"flag"
+	"os"
 )
 
 var targetInt uint = 254
 var dbPath = "dbchain.bolt"
 
+var (
+	printBlocksCmd = flag.NewFlagSet("printchain", flag.ExitOnError)
+	addBlockCmd    = flag.NewFlagSet("addblock", flag.ExitOnError)
+	blockData      = addBlockCmd.String("data", "", "data of new block")
+)
+
 func main() {
+	flag.Parse()
+
 	fmt.Println("hello, blockchain !!!")
+	blockchain, db, err := initBlockchain()
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if db != nil {
+			db.Close()
+		}
+	}()
+
+	switch os.Args[1] {
+	case "printchain":
+		err = printBlocksCmd.Parse(os.Args[2:])
+	case "addblock":
+		err = addBlockCmd.Parse(os.Args[2:])
+	default:
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if printBlocksCmd.Parsed() {
+		blockchain.PrintBlocks()
+		return
+	}
+	if addBlockCmd.Parsed() {
+		if len(*blockData) > 0 {
+			err = blockchain.Add([]byte(*blockData))
+			if err != nil {
+				fmt.Printf("add block failed\n")
+				return
+			} else {
+				fmt.Printf("add block success\n")
+				return
+			}
+		} else {
+			fmt.Printf("no data for block\n")
+		}
+	} else {
+		flag.PrintDefaults()
+		return
+	}
+
+}
+
+func initBlockchain() (*core.Blockchain, *bolt.DB, error) {
 	db, err := bolt.Open("my.db", 0600, nil)
 	if err != nil {
 		fmt.Printf("open db error: %s \n", err)
-		return
+		return nil, nil, err
 	}
-	defer db.Close()
 
 	blockchain, err := core.NewBlockchain(targetInt, db)
 	if err != nil {
 		fmt.Printf("init block chain error: %s\n", err)
-		return
+		return nil, db, err
 	}
 	//err = addTestBlock(blockchain)
 	//if err != nil {
@@ -29,7 +83,7 @@ func main() {
 	//	return
 	//}
 
-	blockchain.PrintBlocks()
+	return blockchain, db, nil
 }
 
 func addTestBlock(blockchain *core.Blockchain) error {
